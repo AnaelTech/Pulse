8f90d931364268c2321f688354ebaae517d998df
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-09 11:59:14 +0200

    show friends user_post.php

--- user_post.php
+++ user_post.php
@@ -1,25 +1,53 @@
 <?php
 require_once __DIR__ . "/layout/header.php";
 require_once __DIR__ . "/layout/navbar.php";
+require_once __DIR__ . "/functions/ConnectDB.php";
+require_once __DIR__ . "/classes/FriendshipsTable.php";
+
+try {
+    $pdo = getDbConnection();
+    $friendsDb = new FriendshipsTable($pdo);
+} catch (PDOException) {
+    echo "Erreur lors de la connexion à la base de données";
+    exit;
+}
 
 if (!isset($_SESSION['userInfos'])) {
     header('Location: home.php');
     exit();
 }
+
+$friends = $friendsDb->findFriends($_SESSION["userInfos"]["id"]);
 ?>
-<section class="section4 mt-4">
+<section id="create-post" class="section4 mt-4">
     <div class="container">
         <div class="row ligne space-between">
             <div class="col-lg">
                 <div class="row ligne space-between">
-                    <div class="col-lg-3">
+                    <div class="col-lg-4">
                         <div class="border-none mb-4">
                             <div class="text-center">
                                 <h3>Friends</h3>
+                                <div class="mb-4 text-center card card-friends p-5">
+                                    <?php
+                                    foreach ($friends as $friend) {
+                                        $image = !empty($friend['user_picture']) ? 'uploads/user/' . $friend['user_picture'] : 'assets/default-img-friends.jpg';
+                                    ?>
+                                        <div class="d-flex align-items-center mb-4">
+                                            <img src="<?php echo $image; ?>" alt="img-user" class="img-fluid rounded-pill img-friends me-3" style="width: 40px; height: 40px;">
+                                            <p class="mb-0">
+                                                <?php echo strtoupper($friend['complet_name']); ?>
+                                                <span class="badge text-bg-secondary rounded-pill ms-3">Offline</span>
+                                            </p>
+                                        </div>
+                                    <?php
+                                    }
+                                    ?>
+                                </div>
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg">
+                    <div class="col-lg-8">
                         <div class="border-none mb-4">
                             <div class="text-center">
                                 <h3>Post</h3>
