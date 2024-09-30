6e81466bf991e3e04fb471b350b0875faff125d1
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-09-30 12:56:19 +0200

    update

--- user_homepage.php
+++ user_homepage.php
@@ -1,9 +1,9 @@
 <?php
+require_once __DIR__ . "/layout/header.php";
 if (empty($_SESSION['userInfos'])) {
     header('Location: home.php');
     exit();
 }
-require_once __DIR__ . "/layout/header.php";
 require_once __DIR__ . "/layout/navbar.php";
 require_once __DIR__ . "/classes/UserPost.php";
 require_once __DIR__ . "/functions/ConnectDB.php";
