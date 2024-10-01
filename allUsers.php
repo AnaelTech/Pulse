08002fec012c2a50dfec769c673851fd40d9363a
AnaelTech <142304772+AnaelTech@users.noreply.github.com>
2024-10-01 15:49:28 +0200

    update search user

--- allUsers.php
+++ allUsers.php
@@ -1,41 +1,37 @@
 <?php
+ob_start();
 require_once __DIR__ . "/layout/header.php";
+if (!isset($_SESSION['userInfos'])) {
+    header('Location: home.php');
+    exit();
+}
+
 require_once __DIR__ . "/layout/navbar.php";
 require_once __DIR__ . "/functions/ConnectDB.php";
 require_once __DIR__ . "/classes/UserSearch.php";
 require_once __DIR__ . "/classes/UserTable.php";
 require_once __DIR__ . "/classes/FriendshipsTable.php";
 
-if (!isset($_SESSION['userInfos'])) {
-    header('Location: home.php');
-    exit();
-}
-
-if (!isset($_GET['search']) && empty($_GET['search'])) {
-    try {
-        $pdo = getDbConnection();
+try {
+    $pdo = getDbConnection();
+    $isFriendDb = new FriendshipsTable($pdo); // Défini ici pour les deux blocs
+    if (!isset($_GET['search']) && empty($_GET['search'])) {
         $users = new UserTable($pdo);
-        $isFriendDb = new FriendshipsTable($pdo);
         $users = $users->findAll();
-    } catch (PDOException $e) {
-        echo "Erreur lors de la connexion à la base de données";
-        exit;
-    }
-} else {
-    // Utiliser la valeur du paramètre de recherche pour effectuer une nouvelle requête et récupérer les résultats
-    try {
+    } else {
         $search = htmlspecialchars($_GET['search']);
-        $pdo = getDbConnection();
         $users = new UserSearch($pdo);
         $users = $users->findByName($search);
-    } catch (Exception $e) {
-        echo "Erreur lors de la connexion à la base de données";
-        exit;
     }
+} catch (PDOException $e) {
+    echo "Erreur lors de la connexion à la base de données";
+    exit;
 }
 
 $currentUserId = $_SESSION['userInfos']['id'];
+ob_end_flush();
 ?>
+
 <section id="all-users" class="mt-5">
     <h1 class="text-center my-4">Résultat de votre recherche</h1>
     <div class="container">
